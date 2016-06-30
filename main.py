a4db37612902016d44b5a9c8d513330bd82d7fed
palloc <palloc.mknet10@gmail.com>
2016-06-30 12:25:49 +0900

    fixed some

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from libDL import *
       
 if __name__ == '__main__':
+
     print "----------------------"
     print "    Start program   "
     print "----------------------"
@@ -15,6 +16,10 @@ if __name__ == '__main__':
     for i in range(len(input_node)):
         w.append(MakeWeight(len(input_node[0])+1, 3))
 
+    Input = Layer()
+    Layer2 = Layer()
+    Layer3 = Layer()
+    Layer4 = Layer()
     #inputの数だけ学習させる
     for z in range(len(input_node)):
         print "----------------------"
@@ -22,21 +27,21 @@ if __name__ == '__main__':
         print "----------------------"
 
         #１層目(入力層)
-        Input = Layer()
         Input.node = input_node[z]
         print "Layer1's node =",
         print Input.node
-        print "---------------------"
-    
         #２層目(中間層)
-        Layer2 = Layer()
         Pass_FC(Input, Layer2, w[0])
+        print "Layer2's node = ",
+        print Layer2.node
         #３層目(中間層)
-        Layer3 = Layer()
         Pass_FC(Layer2, Layer3, w[1])
+        print "Layer3's node = ",
+        print Layer3.node
         #４層目(出力層)
-        Layer4 = Layer()
         Pass_FC_Out(Layer3, Layer4, w[2])
+        print "Layer4's node = " ,
+        print Layer4.node
 
         print "----------------------"
         print "       Fin FFNN       "
@@ -50,20 +55,23 @@ if __name__ == '__main__':
         #d = t_data[z]
         d = [1,0,0]
         delta_4 = First_Delta_Func(Layer4.node, d)
-        w4 = FC_Update_Func(Layer3.node, delta_4, w[2])
+        w[2] = FC_Update_Func(Layer3.node, delta_4, w[2])
         print "new w4 =",
         print w[2]
-        print "---------------------"
+
         #更新後のw4を使用
         delta_3 = Delta_Func(Layer3.node, w[2], delta_4)
-        w3 = FC_Update_Func(Layer2.node, delta_3, w[1])
-        print "new w3=",
+        w[1] = FC_Update_Func(Layer2.node, delta_3, w[1])
+        print "\nnew w3=",
         print w[1]
-        print "---------------------"
+
         #更新後のw3を使用
         delta_2 = Delta_Func(Layer2.node, w[1], delta_3)
-        w3 = FC_Update_Func(Input.node, delta_2, w[0])
-        print "new w2=",
+        w[0] = FC_Update_Func(Input.node, delta_2, w[0])
+        print "\nnew w2=",
         print w[0]
-        print "---------------------"
+
     
+        print "----------------------"
+        print "        Fin BP        "
+        print "----------------------"
