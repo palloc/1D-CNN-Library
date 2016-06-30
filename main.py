92d75c2d6a6dde66ad453b2d8cbdbfae35469511
palloc <palloc.mknet10@gmail.com>
2016-06-30 10:38:33 +0900

    いろいろ関数か

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 #-*- coding=utf8 -*-
-from function import *
+from libDL import *
       
 if __name__ == '__main__':
     print "----------------------"
     print "    Start program   "
     print "----------------------"
-
-    #重み行列の作成
-    MakeWeight = lambda x,y:[[1.0 for i in range(x)] for j in range(y)]
     
     #ファイルからの入力値の読み取り
-    input_file = open("data")
-    temp = input_file.read().split('\n')
-    input_node = []
-    for i in temp:
-        if i != '':
-            input_node.append(map(int, i.split(',')))
+    file_name = sys.argv[1]
+    input_node = open_data(file_name)
     
-    #すべて重み1の行列(２次元配列)を様々な大きさで用意
-    w1 = MakeWeight(len(input_node[0])+1, 3)
-    w2 = MakeWeight(len(input_node[0])+1, 3)
-    w3 = MakeWeight(len(input_node[0])+1, 3)
-    w4 = MakeWeight(len(input_node[0])+1, 3)
+    #すべて重み1の行列(２次元配列)を用意
+    w = []
+    for i in range(len(input_node)):
+        w.append(MakeWeight(len(input_node[0])+1, 3))
 
     #inputの数だけ学習させる
     for z in range(len(input_node)):
@@ -38,13 +30,13 @@ if __name__ == '__main__':
     
         #２層目(中間層)
         Layer2 = Layer()
-        Pass_FC(Input, Layer2, w2)
+        Pass_FC(Input, Layer2, w[0])
         #３層目(中間層)
         Layer3 = Layer()
-        Pass_FC(Layer2, Layer3, w3)
+        Pass_FC(Layer2, Layer3, w[1])
         #４層目(出力層)
         Layer4 = Layer()
-        Pass_FC_Out(Layer3, Layer4, w4)
+        Pass_FC_Out(Layer3, Layer4, w[2])
 
         print "----------------------"
         print "       Fin FFNN       "
@@ -58,26 +50,20 @@ if __name__ == '__main__':
         #d = t_data[z]
         d = [1,0,0]
         delta_4 = First_Delta_Func(Layer4.node, d)
-        print "delta4 =",
-        print delta_4
-        w4 = FC_Update_Func(Layer3.node, delta_4, w4)
+        w4 = FC_Update_Func(Layer3.node, delta_4, w[2])
         print "new w4 =",
-        print w4
+        print w[2]
         print "---------------------"
         #更新後のw4を使用
-        delta_3 = Delta_Func(Layer3.node, w4, delta_4)
-        print "delta3 =",
-        print delta_3
-        w3 = FC_Update_Func(Layer2.node, delta_3, w3)
+        delta_3 = Delta_Func(Layer3.node, w[2], delta_4)
+        w3 = FC_Update_Func(Layer2.node, delta_3, w[1])
         print "new w3=",
-        print w3
+        print w[1]
         print "---------------------"
         #更新後のw3を使用
-        delta_2 = Delta_Func(Layer2.node, w3, delta_3)
-        print "delta2 =",
-        print delta_2
-        w3 = FC_Update_Func(Input.node, delta_2, w2)
+        delta_2 = Delta_Func(Layer2.node, w[1], delta_3)
+        w3 = FC_Update_Func(Input.node, delta_2, w[0])
         print "new w2=",
-        print w2
+        print w[0]
         print "---------------------"
     
