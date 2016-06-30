4c14241aa43d0b349f69cf027ef40e125a2cadf5
palloc <palloc.mknet10@gmail.com>
2016-06-30 10:44:25 +0900

    ライブラリを分割

---
+++ libDL.py
@@ -0,0 +1,95 @@
+#-*- coding=utf8 -*-
+from CalcFunc import *
+
+
+#FC層の計算関数
+def Pass_FC(old_layer, new_layer, w):
+    old_layer.node.append(1)
+    print w
+    new_layer.node = FullConect_Func(old_layer.node, w)
+    new_layer.Do_Logistic()
+    print "Layer%d's node = " % new_layer.layer_id,
+    print new_layer.node
+    print "-----------------------------\n"
+
+#出力層のFC関数
+def Pass_FC_Out(old_layer, new_layer, w):
+    old_layer.node.append(1)
+    print w
+    new_layer.node = FullConect_Func(old_layer.node, w)
+    new_layer.Do_Softmax()
+    print "Layer%d's node = " % new_layer.layer_id,
+    print new_layer.node
+    print "-----------------------------\n"
+
+"""
+--------------------------------------
+     バックプロバケーション用の関数
+--------------------------------------
+"""
+
+#ロジスティック関数の微分
+def Dif_Logistic_Func(x):
+    new_node = []
+    for i in x:
+        new_node.append(Logistic_Func(i) * (1 - Logistic_Func(i)))
+    return new_node
+
+#最初のクロスエントロピーの微分δの計算
+def First_Delta_Func(x, d):
+    result = []
+    for i in range(len(x)):
+        result.append( x[i] - d[i] )
+    return result
+
+#各層のδの計算
+def Delta_Func(x, w, old_delta):
+    new_delta = []
+    temp_s = []
+    #ロジスティックの微分に通す
+    x = Dif_Logistic_Func(x)
+    #(W,δ)
+    for i in range(len(w)):
+        temp_t = 0
+        for j in range(len(w[0])):
+            temp_t += w[i][j] * old_delta[i]
+            temp_s.append(temp_t)
+    #Δ=f'(x)・temp_s
+    for i in range(len(x)):
+        new_delta.append(x[i] * temp_s[i])
+    return new_delta
+
+#出力層の重みの更新
+def FC_Update_Func(x, delta, w):
+    new_w = []
+    for i in range(len(w)):
+        temp = []
+        for j in range(len(w[i])):
+            temp.append(w[i][j] - delta[i] * x[j])
+        new_w.append(temp)
+    return new_w
+
+
+#layerクラス
+class Layer:
+    """
+    ノードを保有する層を作るクラス
+    """
+    #何層目かを表すid
+    layer_id = 1
+    #コンストラクタ
+    def __init__(self):
+        #ノード情報を保持する配列
+        self.node = []
+        Layer.layer_id += 1
+    #全ノードをロジスティック関数に通す
+    def Do_Logistic(self):
+        self.node = map(Logistic_Func, self.node)
+    #ソフトマックス関数(出力層での活性化関数)
+    def Softmax_Func(self, x):
+        return math.exp(x) / sum(map(math.exp, self.node))
+    #ソフトマックス関数を全ノードに通す
+    def Do_Softmax(self):
+        Sum = sum(map(math.exp, self.node))
+        temp = lambda x:math.exp(x)/Sum
+        self.node = map(temp, self.node)
